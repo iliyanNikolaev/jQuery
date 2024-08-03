0860e5ba53570ecbd9f8510888108674bda0777d
Iliyan Nikolaev <ilichVIVA@gmail.com>
2024-08-03 12:28:11 +0300

    selectors

---
+++ script.js
@@ -0,0 +1,6 @@
+$('h2').hide()
+
+$('#list li:first').css('color', 'blue')
+$('#list li:last').css('color', 'purple')
+$('#list li:odd').css('background-color', 'yellow')
+$('#list li:even').css('background-color', 'orange')
